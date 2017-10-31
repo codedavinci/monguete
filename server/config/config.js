19a843a32bca2b3e597a14cfaf1f632f5668b598
mac <codedavinci.now@gmail.com>
2017-10-31 04:29:00 -0700

    Remove hard-coded URI MongoDb

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,6 @@
 const env = process.env.NODE_ENV || 'development'
 const local = 'mongodb://localhost:27017'
 
-console.log("env *****", env)
-
 if (env === 'development') {
   process.env.PORT = 3000
   process.env.MONGODB_URI = `${local}/todo`
